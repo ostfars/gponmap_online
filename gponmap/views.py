dfc4f01686f6967f68a8361b4a236280346f52d2
Vladimir Minin <v_minin@rambler.ru>
2023-11-04 11:40:11 +0300

    poly

--- gponmap/views.py
+++ gponmap/views.py
@@ -504,10 +504,10 @@ def google_earth_kml(request):
     polygon_layer = QgisPolygon.objects.annotate(geom_kml=AsKML('geom'))
     for obj in polygon_layer:
         kml += '<Placemark>'
-        # kml += '<Description><![CDATA['
-        # if obj.p_oskm:
+        # kml += '<description><![CDATA['
+        # if obj.p_oskm is not None:
         #     kml += obj.p_oskm
-        # kml += ']]</Description)>'
+        # kml += ']]</description>'
         kml += '<Style><PolyStyle><color>B3' + obj.color[1:] + '</color><outline>1</outline></PolyStyle></Style>'
         kml += '{}</Placemark>'.format(obj.geom_kml)
     kml += '</Folder>'
